0be918e17369486680427730fc3f710de92fc990
Kyle <kylepw@gmail.com>
2019-06-18 22:03:14 +0900

    Add Person model properties.

--- sns/models.py
+++ sns/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    facebook_acct = models.CharField(max_length=50)
+    instagram_acct = models.CharField(max_length=30)
+    reddit_acct = models.CharField(max_length=20)
+    spotify_acct = models.CharField(max_length=30)
+    twitter_acct = models.CharField(max_length=20)
