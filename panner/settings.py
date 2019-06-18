d55ba9b987c11f9fc48568a85f7f16d68a0a9d2e
Kyle <kylepw@gmail.com>
2019-06-18 16:05:48 +0900

    Connect to postgresql database.

--- panner/settings.py
+++ panner/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'panner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
