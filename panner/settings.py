6baacbea32850adba42628ffefd970f00f5a47a9
Kyle <kylepw@gmail.com>
2019-06-19 10:57:38 +0900

    Make model migrations.

--- panner/settings.py
+++ panner/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sns.apps.SnsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- sns/models.py
+++ sns/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 class Person(models.Model):
     name = models.CharField(max_length=20)
-    facebook_acct = models.CharField(max_length=50)
-    instagram_acct = models.CharField(max_length=30)
-    reddit_acct = models.CharField(max_length=20)
-    spotify_acct = models.CharField(max_length=30)
-    twitter_acct = models.CharField(max_length=20)
+    facebook = models.CharField(max_length=50, blank=True)
+    instagram = models.CharField(max_length=30, blank=True)
+    reddit = models.CharField(max_length=20, blank=True)
+    spotify = models.CharField(max_length=30, blank=True)
+    twitter = models.CharField(max_length=20, blank=True)
+
+    def __str__(self):
+        return self.name
