5fdf527187f49dc9cea005e558ae391ad7ec1fe3
Kyle <kylepw@gmail.com>
2019-06-20 18:13:58 +0900

    Get secret key from env variable.

--- panner/settings.py
+++ panner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q!e&m(+(q8fd$7%%f-xqxs21!01v2(9*3wn7=l%r!3hg372lfx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
