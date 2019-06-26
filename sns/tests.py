3bd0c35eaf72b918d7a2f9b18992acc986c2dc2b
Kyle <kylepw@gmail.com>
2019-06-26 15:34:06 +0900

    Add tests

--- sns/tests.py
+++ sns/tests.py
@@ -1,12 +1,13 @@
 from django.db.utils import DataError, IntegrityError
 from django.test import TestCase
 from django.urls import reverse
+from django.urls.exceptions import NoReverseMatch
 
 from .forms import ProfileForm
 from .models import Profile
 
 
-class ProfileListViewTests(TestCase):
+class ProfileListViewWithDataTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
@@ -38,6 +39,11 @@ class ProfileListViewTests(TestCase):
         self.assertTrue('is_paginated' in response.context)
         self.assertTrue(response.context['is_paginated'])
         self.assertTrue(len(response.context['profiles']) == 20)
+
+    def test_no_profiles_msg_not_displayed(self):
+        response = self.client.get(self.first_page)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, 'No profiles to display')
 
     def test_lists_all_profiles(self):
         # Second page should show last five profiles
@@ -94,6 +100,60 @@ class ProfileListViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(response, name)
 
+
+class ProfileListViewWithoutDataTests(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get(reverse('profile-list'))
+
+    def test_message_displayed_when_no_profile_data(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertContains(self.response, 'No profiles to display.')
+
+    def test_uses_correct_template(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTemplateUsed(self.response, 'sns/profile_list.html')
+
+    def test_no_profiles_passed_in_context(self):
+        self.assertEqual(self.response.status_code, 200)
+        self.assertTrue('profiles' in self.response.context)
+        self.assertEqual(len(self.response.context['profiles']), 0)
+
+
+class ProfileDetailView(TestCase):
+
+    def setUp(self):
+        self.profile = Profile.objects.create(
+            name='Harry',
+            facebook='fb@fb.com',
+            twitter='hry318'
+        )
+
+    def test_view_url_exists_at_desired_location(self):
+        pk = self.profile.pk
+        self.assertEqual(reverse('profile-detail', kwargs={'pk': pk}), f'/profile/{pk}/')
+
+    def test_view_renders_an_existing_profile(self):
+        pk = self.profile.pk
+        response = self.client.get(reverse('profile-detail', kwargs={'pk': pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('profile' in response.context)
+        self.assertEqual(response.context['profile'], self.profile)
+
+    def test_view_uses_correct_template(self):
+        pk = self.profile.pk
+        response = self.client.get(reverse('profile-detail', kwargs={'pk': pk}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'sns/profile_detail.html')
+
+    def test_does_not_render_nonexisting_profile(self):
+        response = self.client.get(reverse('profile-detail', kwargs={'pk': 100000}))
+        self.assertEqual(response.status_code, 404)
+
+    def test_does_not_render_page_without_pk_value_passed(self):
+        with self.assertRaises(NoReverseMatch):
+            self.client.get(reverse('profile-detail'))
 
 class ProfileModelTests(TestCase):
 
