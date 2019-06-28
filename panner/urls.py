f1b06393981df3c7fe980caa72ee3de521c68e97
Kyle <kylepw@gmail.com>
2019-06-28 14:31:58 +0900

    Fix LINE and Meetup urls

--- panner/urls.py
+++ panner/urls.py
@@ -19,6 +19,8 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('sns.urls')),
     path('admin/', admin.site.urls),
+    path('line/', include('line.urls')),
+    path('meetup/', include('meetup.urls')),
     path('reddit/', include('reddit.urls')),
     path('spotify/', include('spotify.urls')),
     path('twitter/', include('twitter.urls')),
