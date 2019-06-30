a70e24562e7c0159fa492e7800ba79326b34587b
Kyle <kylepw@gmail.com>
2019-06-30 14:12:53 +0900

    Update form fields to reflect model field changes

--- sns/forms.py
+++ sns/forms.py
@@ -6,4 +6,4 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['name', 'reddit', 'spotify', 'twitter']
+        fields = ['name', 'line', 'meetup', 'reddit', 'spotify', 'twitter']
