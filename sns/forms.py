a15981fdffc6e22b02b38accd5a41ab8835e9f90
Kyle <kylepw@gmail.com>
2019-07-15 17:19:41 +0900

    All pages in containers

--- sns/forms.py
+++ sns/forms.py
@@ -4,6 +4,12 @@ from .models import Profile
 
 
 class ProfileForm(forms.ModelForm):
+    # Bootstrap form input elements.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for _, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+
     class Meta:
         model = Profile
         fields = ['name', 'meetup', 'reddit', 'spotify', 'twitter']
