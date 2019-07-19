39b7c2642674d4f5b8d862a0d195bae4e589d8ee
Kyle <kylepw@gmail.com>
2019-07-19 11:07:08 +0900

    Allow 0.0.0.0:8000 as host

--- panner/settings.py
+++ panner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0:8000']
 
 
 # Application definition
