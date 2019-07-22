d9c3065b904210ec64a7e93884b8326501723e4e
Kyle <kylepw@gmail.com>
2019-07-22 19:11:59 +0900

    Change allowed hosts to `localhost`

--- panner/settings.py
+++ panner/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'foo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
