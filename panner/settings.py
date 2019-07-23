7b0001098763a6a85e005b666b027f0fe47d6fa6
Kyle <kylepw@gmail.com>
2019-07-23 10:57:53 +0900

    Always restart containers. Stop using postgres password

--- panner/settings.py
+++ panner/settings.py
@@ -78,9 +78,8 @@ DATABASES = {
         'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
         'NAME': os.environ.get('DB_NAME', 'postgres'),
         'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASS', ''),
         'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'PORT': os.environ.get('DB_PORT', 5432),
     }
 }
 
