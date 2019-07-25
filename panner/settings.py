c5de4bb5354ddacff3fa1a2ca6f24ae5dfe642d9
Kyle <kylepw@gmail.com>
2019-07-26 00:11:45 +0900

    Add redis

--- panner/settings.py
+++ panner/settings.py
@@ -82,6 +82,21 @@ DATABASES = {
 }
 
 
+# Cache
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': os.environ.get('REDIS_URL', 'redis://127.0.0.1:6379/1'),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'SOCKET_CONNECT_TIMEOUT': 5,
+            'SOCKET_TIMEOUT': 5,
+        }
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
