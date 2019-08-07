95c53f8fdbcb16803d50533d137aed646eedbb79
Kyle <kylepw@gmail.com>
2019-08-07 21:44:31 +0900

    Split reddit tests. More refactoring

--- sns/api/test_reddit.py
+++ sns/api/test_reddit.py
@@ -8,17 +8,7 @@ from praw.models import Comment, Submission
 logger = logging.getLogger('reddit')
 
 
-class RedditTests(TestCase):
-
-    def setUp(self):
-        patch_datetime = patch('reddit.datetime')
-        patch_pytz = patch('reddit.pytz')
-
-        self.mock_datetime = patch_datetime.start()
-        self.mock_pytz = patch_pytz.start()
-
-        self.addCleanup(patch_datetime.stop)
-        self.addCleanup(patch_pytz.stop)
+class PrawTests(TestCase):
 
     def test_praw_reddit_called_when_making_instance(self):
         with patch('reddit.PrawReddit') as MockPrawReddit:
@@ -33,12 +23,13 @@ class RedditTests(TestCase):
             client_id='id', client_secret='secret', user_agent='agent', read_only=True
         )
 
-    def test_get_comments_submissions_num_of_results(self):
-        username = 'joe'
-        num = 10
 
-        mock_comment = MagicMock()
-        mock_submission = MagicMock()
+class GetCommentsSubmissionsTest(TestCase):
+
+    def setUp(self):
+        # mock self.api
+        mock_comment = Mock()
+        mock_submission = Mock()
         comment_attrs = {
             'link_title': 'foo',
             'body_html': 'foo',
@@ -55,20 +46,30 @@ class RedditTests(TestCase):
         }
         mock_comment.configure_mock(**comment_attrs)
         mock_submission.configure_mock(**submission_attrs)
-
         mock_comments = MagicMock()
         mock_submissions = MagicMock()
+        mock_comments.new.return_value = iter([mock_comment]*20)
+        mock_submissions.new.return_value = iter([mock_submission]*20)
+        self.mock_api = MagicMock()
+        self.mock_api.redditor.return_value.comments = mock_comments
+        self.mock_api.redditor.return_value.submissions = mock_submissions
 
-        mock_comments.new.return_value = iter([mock_comment]*num)
-        mock_submissions.new.return_value = iter([mock_submission]*num)
+        patch_datetime = patch('reddit.datetime')
+        patch_pytz = patch('reddit.pytz')
 
-        mock_api = MagicMock()
-        mock_api.redditor.return_value.comments = mock_comments
-        mock_api.redditor.return_value.submissions = mock_submissions
+        self.mock_datetime = patch_datetime.start()
+        self.mock_pytz = patch_pytz.start()
+
+        self.addCleanup(patch_datetime.stop)
+        self.addCleanup(patch_pytz.stop)
+
+    def test_comments_submissions_calls(self):
+        username = 'joe'
+        num = 5
 
         r = Reddit('id', 'secret', 'agent')
-        r.api = mock_api
-        print(len(r.get_comments_submissions(username, num=num)))
+        r.api = self.mock_api
+        r.get_comments_submissions(username, num=num)
 
         chained_comments = call.redditor(username).comments.new(limit=num)
         chained_submissions = call.redditor(username).submissions.new(limit=num)
@@ -76,9 +77,14 @@ class RedditTests(TestCase):
         call_list_comments = chained_comments.call_list()
         call_list_submissions = chained_submissions.call_list()
 
-        mock_api.assert_has_calls(call_list_comments)
-        mock_api.assert_has_calls(call_list_submissions)
+        self.mock_api.assert_has_calls(call_list_comments)
+        self.mock_api.assert_has_calls(call_list_submissions)
 
-        mock_comments.new.assert_called_with(limit=num)
+    def test_num_of_results_5(self):
+        num = 5
 
+        r = Reddit('id', 'secret', 'agent')
+        r.api = self.mock_api
+
+        self.assertEqual(len(r.get_comments_submissions('joe', num=num)), 5)
 
