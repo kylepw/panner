972a7832fbe5819b176f95e3781eafad4716d388
Kyle <kylepw@gmail.com>
2019-08-12 16:36:10 +0900

    Create meetup and spotify test files

---
+++ sns/api/spotify/test_spotify.py
@@ -0,0 +1,14 @@
+from api import Spotify
+from auth import OAuth2Client
+
+from unittest import TestCase
+from unittest.mock import Mock
+
+
+class TestSpotify(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
