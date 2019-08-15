bc4f504d942a0df76e1cd7eedf628a610ae6e988
Kyle <kylepw@gmail.com>
2019-08-16 00:36:33 +0900

    Move spotify tests to separate dir

---
+++ sns/api/spotify/tests/test_api.py
@@ -0,0 +1,60 @@
+from ..api import requests, Spotify
+
+from unittest import TestCase
+from unittest.mock import patch, Mock
+
+
+class TestApi(TestCase):
+
+    def setUp(self):
+        self.spotify = Spotify(auth=Mock())
+
+        patch_get = patch.object(requests, 'get')
+        self.mock_get = patch_get.start()
+        self.addCleanup(patch_get.stop)
+
+    def test_get_playlists_with_valid_id(self):
+        response = {'items': ['foobar']}
+        self.mock_get.return_value.json.return_value = response
+        self.assertIn('items', self.spotify.get_playlists('valid_id'))
+
+    def test_get_playlists_with_invalid_id(self):
+        response = {'error': '401'}
+        self.mock_get.return_value.json.return_value = response
+        self.assertIn('error', self.spotify.get_playlists('invalid_id'))
+
+    def test_get_playlists_with_no_id(self):
+        # Should error in read-only mode
+        response = {'error': '404'}
+        self.mock_get.return_value.json.return_value = response
+        self.assertIn('error', self.spotify.get_playlists())
+
+    def test_get_profile_with_valid_id(self):
+        response = {'display_name': 'valid_id'}
+        self.mock_get.return_value.json.return_value = response
+        self.assertIn('display_name', self.spotify.get_profile('valid_id'))
+
+    def test_get_profile_with_invalid_id(self):
+        response = {'error': '401'}
+        self.mock_get.return_value.json.return_value = response
+        self.assertIn('error', self.spotify.get_profile('invalid_id'))
+
+    def test_get_profile_with_no_id(self):
+        # Should error in read-only mode
+        response = {'error': '404'}
+        self.mock_get.return_value.json.return_value = response
+        self.assertIn('error', self.spotify.get_profile())
+
+    @patch.object(Spotify, 'get_profile', return_value={'images':[{'url': 'http://foo.com/image.jpg'}]})
+    def test_profile_image_url_with_valid_get_profile_return_value(self, mock_profile_image_url):
+        self.assertEqual(self.spotify.profile_image_url('badid'), 'http://foo.com/image.jpg')
+
+    @patch.object(Spotify, 'get_profile', return_value={'error':'404'})
+    def test_profile_image_url_with_get_profile_fail(self, mock_profile_image_url):
+        self.assertEqual(self.spotify.profile_image_url('badid'), None)
+
+
+class TestAuth(TestCase):
+
+    def test_something(self):
+        pass
---
+++ sns/api/spotify/tests/test_auth.py
@@ -0,0 +1,124 @@
+from collections import namedtuple
+from django.test import RequestFactory
+from unittest import TestCase
+from unittest.mock import MagicMock, Mock, patch
+
+from ..auth import logging, OAuth2Bearer, OAuthHandler, OAuth2Code, OAuth2Client, OAuth2Session, requests, time
+
+logger = logging.getLogger('spotify.auth')
+
+# mocked request obj
+Request = namedtuple('Request', 'headers, session')
+
+
+class TestOAuth2Bearer(TestCase):
+
+    def setUp(self):
+        self.request = Request(headers={}, session={})
+
+    def test_call_applies_header(self):
+        token = '23l3kjlk'
+        self.assertIsNone(self.request.headers.get('Authorization'))
+        self.request = OAuth2Bearer(token)(self.request)
+        self.assertEqual(self.request.headers.get('Authorization'), 'Bearer ' + token)
+
+
+class TestOAuthHandler(TestCase):
+
+    def setUp(self):
+        token = {'access_token': 'lkj23'}
+        self.oauth = OAuthHandler(client_id='id', client_secret='secret', token=token)
+
+    def test_is_token_expired_with_expires_at_in_token(self):
+        self.oauth.token['expires_at'] = 1565872294
+        self.assertTrue(self.oauth.is_token_expired())
+
+    def test_is_token_expired_without_expires_at_in_token(self):
+        self.assertTrue(self.oauth.is_token_expired())
+
+    def test_is_token_expired_with_expires_at_str(self):
+        self.oauth.token['expires_at'] = '1565872294'
+        self.assertTrue(self.oauth.is_token_expired())
+
+    def test_is_token_expired_with_expires_token_argument(self):
+        token = {'access_token': 'dfoasduf9', 'expires_at': 1565872294}
+        self.assertTrue(self.oauth.is_token_expired(token))
+
+
+class TestOAuth2Code(TestCase):
+
+    def setUp(self):
+        patch_oauth2session = patch('spotify.auth.OAuth2Session')
+        self.mock_oauth2session = patch_oauth2session.start()
+
+        self.addCleanup(patch_oauth2session.stop)
+
+    def test_instance_without_token_passed(self):
+        oauth = 'oauth object'
+        self.mock_oauth2session.return_value = oauth
+
+        self.assertEqual(
+            OAuth2Code(client_id='id', client_secret='secret', redirect_uri='http://foobar').oauth,
+            oauth
+        )
+        self.mock_oauth2session.assert_called_once_with('id', redirect_uri='http://foobar', scope=None)
+
+    def test_instance_with_token_passed(self):
+        oauth = 'oauth object'
+        token = {'access_token': 'dfoasduf9', 'expires_at': 1565872294}
+        self.mock_oauth2session.return_value = oauth
+
+        auth = OAuth2Code(client_id='id', client_secret='secret', redirect_uri='http://foobar', token=token)
+        self.assertEqual(auth.oauth, oauth)
+        self.assertEqual(auth.token, token)
+        self.mock_oauth2session.assert_called_once_with('id', token=token)
+
+    def test_get_access(self):
+        pass
+
+    def test_refresh_token(self):
+        pass
+
+
+class TestOAuth2Client(TestCase):
+
+    def setUp(self):
+        patch_post = patch.object(requests, 'post')
+        self.mock_post = patch_post.start()
+
+        self.token = {
+            'access_token': 'BQDf7Lr1r',
+            'token_type': 'Bearer',
+            'expires_in': 3600,
+            'scope': '',
+            'expires_at': 1565878190.534921,
+        }
+
+        self.addCleanup(patch_post.stop)
+
+    def test_valid_token_response(self):
+        self.mock_post.return_value = Mock(json=Mock(return_value=self.token))
+        self.assertEqual(OAuth2Client(client_id='id', client_secret='secret').token, self.token)
+
+    def test_error_response(self):
+        self.mock_post.return_value = Mock(json=Mock(return_value={'error': '401', 'error_description': 'You effed up'}))
+        with self.assertRaises(ValueError) as e:
+            OAuth2Client(client_id='id', client_secret='secret')
+        self.assertEqual(str(e.exception), 'Failed to acquire access token with `401` error: You effed up')
+
+    def test_no_token_type_key(self):
+        del self.token['token_type']
+        self.mock_post.return_value = Mock(json=Mock(return_value=self.token))
+        with self.assertRaises(ValueError) as e:
+            OAuth2Client(client_id='id', client_secret='secret')
+        self.assertEqual(str(e.exception), 'Expected token_type to equal "bearer" but instead got None')
+
+    @patch.object(logger, 'debug')
+    def test_token_without_expires_at_val_is_given_one(self, mock_debug):
+        del self.token['expires_at']
+        self.mock_post.return_value = Mock(json=Mock(return_value=self.token))
+        self.assertTrue(OAuth2Client(client_id='id', client_secret='secret').token)
+        mock_debug.assert_called_once()
+
+
+
